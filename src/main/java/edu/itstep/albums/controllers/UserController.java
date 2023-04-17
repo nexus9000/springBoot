fbd544a4f1b582cdacd693430cf2acf7bb203d0b
User <java.user.tester@gmail.com>
2023-04-17 18:32:48 +0300

    spring thymeleaf

--- src/main/java/edu/itstep/albums/controllers/UserController.java
+++ src/main/java/edu/itstep/albums/controllers/UserController.java
@@ -1,6 +1,6 @@
 package edu.itstep.albums.controllers;
 
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,24 +24,35 @@ public class UserController {
    }
    @GetMapping("/")
    public String showUserList(Model model) {
+	   
        model.addAttribute("users", userRepository.findAll());
        return "index";
    }
    @GetMapping("/edit/{id}")
-   public String showUpdate(@PathVariable("id") long id, Model model) {
-	   Optional<User> user = Optional.ofNullable(
-			   userRepository.findById(id).
-			   orElseThrow(()->new IllegalArgumentException("Invalid user id: "+id)));
-	   model.addAttribute("user",user);
-	   return "/update-user";
-	   
+   public String showUpdateForm(@PathVariable("id") long id, Model model) {
+       User user = userRepository.findById(id)
+         .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+       
+       model.addAttribute("user", user);
+       return "update-user";
+   }
+   @PostMapping("/update/{id}")
+   public String updateUser(@PathVariable("id") long id, @Valid User user, 
+     BindingResult result, Model model) {
+       if (result.hasErrors()) {
+           user.setId(id);
+           return "update-user";
+       }
+           
+       userRepository.save(user);
+       return "redirect:/";
    }
    @GetMapping("/delete/{id}")
    public String deleteUser(@PathVariable("id")long id, Model model)throws Exception{
 	   User user =  userRepository.findById(id).
 			   orElseThrow(()->new IllegalArgumentException("Invalid user id: "+id));
 	   userRepository.delete(user);
-	   return "redirect:/index";
+	   return "redirect:/";
    }
    
    @PostMapping("/adduser")
@@ -50,6 +61,6 @@ public class UserController {
 		   return "add-user";
 	   }
 	   userRepository.save(user);
-	   return "redirect:/index";
+	   return "redirect:/";
    }
 }
