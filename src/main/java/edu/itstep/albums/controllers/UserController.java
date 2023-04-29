6b0bcb2275b718fb9121671d6f4335d32bfce579
User <java.user.tester@gmail.com>
2023-04-29 11:57:40 +0300

    modal added

--- src/main/java/edu/itstep/albums/controllers/UserController.java
+++ src/main/java/edu/itstep/albums/controllers/UserController.java
@@ -1,7 +1,5 @@
 package edu.itstep.albums.controllers;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,56 +16,59 @@ import jakarta.validation.Valid;
 public class UserController {
 	@Autowired
 	UserRepository userRepository;
-   @GetMapping("/signup")
-   public String signUpForm(User user) {
-	   return "add-user";
-   }
-   @GetMapping("/")
-   public String showUserList(Model model) {
-	   
-       model.addAttribute("users", userRepository.findAll());
-       return "index";
-   }
-   @GetMapping("/login")
-   public String log(Model model) {
-	   
-      
-       return "login";
-   }
-   @GetMapping("/edit/{id}")
-   public String showUpdateForm(@PathVariable("id") long id, Model model) {
-       User user = userRepository.findById(id)
-         .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-       
-       model.addAttribute("user", user);
-       return "update-user";
-   }
-   
-   @PostMapping("/update/{id}")
-   public String updateUser(@PathVariable("id") long id, @Valid User user, 
-     BindingResult result, Model model) {
-       if (result.hasErrors()) {
-           user.setId(id);
-           return "update-user";
-       }
-           
-       userRepository.save(user);
-       return "redirect:/";
-   }
-   @GetMapping("/delete/{id}")
-   public String deleteUser(@PathVariable("id")long id, Model model)throws Exception{
-	   User user =  userRepository.findById(id).
-			   orElseThrow(()->new IllegalArgumentException("Invalid user id: "+id));
-	   userRepository.delete(user);
-	   return "redirect:/";
-   }
-   
-   @PostMapping("/adduser")
-   public String addUser(@Valid User user, BindingResult result , Model model) {
-	   if(result.hasErrors()) {
-		   return "add-user";
-	   }
-	   userRepository.save(user);
-	   return "redirect:/";
-   }
+
+	@GetMapping("/signup")
+	public String signUpForm(User user) {
+		return "add-user";
+	}
+
+	@GetMapping("/")
+	public String showUserList(Model model) {
+
+		model.addAttribute("users", userRepository.findAll());
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String log(Model model) {
+
+		return "login";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String showUpdateForm(@PathVariable("id") long id, Model model) {
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+		model.addAttribute("user", user);
+		return "update-user";
+	}
+
+	@PostMapping("/update/{id}")
+	public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			user.setId(id);
+			return "update-user";
+		}
+
+		userRepository.save(user);
+		return "redirect:/";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) throws Exception {
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid user id: " + id));
+		userRepository.delete(user);
+		return "redirect:/";
+	}
+
+	@PostMapping("/adduser")
+	public String addUser(@Valid User user, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "index";
+		}
+		userRepository.save(user);
+		return "index";
+	}
 }
