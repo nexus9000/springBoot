3b83091809ccb02117f757ae1717a17acbf963bf
User <java.user.tester@gmail.com>
2023-05-19 19:49:46 +0300

    Transaction added

--- src/main/java/edu/itstep/albums/blockchain/Transaction.java
+++ src/main/java/edu/itstep/albums/blockchain/Transaction.java
@@ -1,5 +1,24 @@
 package edu.itstep.albums.blockchain;
 
-public class Transaction {
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Transaction {
+	//id of the transaction
+   private String transactionId;
+   private PublicKey  sender;
+   private PublicKey receiver;
+   private double amount;
+   //transaction must be signed
+   private byte[] signature;
+   public List<TransactionInput> inputs;
+   public List<TransactionOutput> outputs;
+   
+   public Transaction(PublicKey sender, PublicKey receiver, 
+		   double amount, List<TransactionInput> inputs) {
+	   this.inputs = new ArrayList<>();
+	   this.outputs = 
+   }
+   
 }
