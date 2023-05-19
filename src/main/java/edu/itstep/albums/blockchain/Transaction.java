df9c2071a669862f9e044aa38a77b76d5e4d3d26
User <java.user.tester@gmail.com>
2023-05-19 20:42:28 +0300

    Transaction added

--- src/main/java/edu/itstep/albums/blockchain/Transaction.java
+++ src/main/java/edu/itstep/albums/blockchain/Transaction.java
@@ -1,10 +1,11 @@
 package edu.itstep.albums.blockchain;
 
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Transaction {
+public final class Transaction {
 	//id of the transaction
    private String transactionId;
    private PublicKey  sender;
@@ -18,7 +19,28 @@ public class Transaction {
    public Transaction(PublicKey sender, PublicKey receiver, 
 		   double amount, List<TransactionInput> inputs) {
 	   this.inputs = new ArrayList<>();
-	   this.outputs = 
+	   this.outputs = new ArrayList<>();
+	   this.sender = sender;
+	   this.receiver = receiver;
+	   this.inputs = inputs;
+	   calculateHash();
+   }
+   
+   public void generateSignature(PrivateKey privateKey) {
+	   String data = sender.toString() +
+			   receiver.toString() + Double.toString(amount);
+	   signature = CryptographyHelper.sign(privateKey, data);
+   }
+   public boolean verifySignature() {
+	   String data = sender.toString() +
+			   receiver.toString() + Double.toString(amount);
+	   return CryptographyHelper.verify(sender, data, signature);
+   }
+   
+   private final void calculateHash() {
+	   String hashData = sender.toString() +
+			   receiver.toString() + Double.toString(amount);
+	   this.transactionId = CryptographyHelper.generateHash(hashData);
    }
    
 }
--- src/main/java/edu/itstep/albums/blockchain/TransactionInput.java
+++ src/main/java/edu/itstep/albums/blockchain/TransactionInput.java
@@ -1,5 +1,30 @@
 package edu.itstep.albums.blockchain;
 
 public class TransactionInput {
+	
+	private String transactionOutputId;
+	// unspent transaction output
+	private TransactionOutput UTXO;
+
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+
+	}
+
+	public String getTransactionOutputId() {
+		return transactionOutputId;
+	}
+
+	public void setTransactionOutputId(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+
+	public TransactionOutput getUTXO() {
+		return UTXO;
+	}
+
+	public void setUTXO(TransactionOutput uTXO) {
+		UTXO = uTXO;
+	}
 
 }
