3bb5030ec81ea0408030e8bfd34b54a0b906b5dc
User <java.user.tester@gmail.com>
2023-05-19 20:26:23 +0300

    Transaction added

--- src/main/java/edu/itstep/albums/blockchain/CryptographyHelper.java
+++ src/main/java/edu/itstep/albums/blockchain/CryptographyHelper.java
@@ -2,7 +2,11 @@ package edu.itstep.albums.blockchain;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.Security;
+import java.security.Signature;
 import java.security.spec.ECGenParameterSpec;
 
 public class CryptographyHelper {
@@ -15,11 +19,51 @@ public class CryptographyHelper {
 	public static KeyPair ellipticCurveCrypto() {
 		try {
 			Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA","BC");
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
 			ECGenParameterSpec params = new ECGenParameterSpec("prime256v1");
 			keyPairGenerator.initialize(params);
 			return keyPairGenerator.generateKeyPair();
 		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static final byte[] sign(PrivateKey privateKey, String input) {
+		Signature signature;
+		byte[] output = new byte[0];
+		try {
+			signature = Signature.getInstance("ECDSA", "BC");
+			signature.initSign(privateKey);
+			signature.update(input.getBytes());
+			output = signature.sign();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return output;
+	}
+
+	public final static boolean verify(PublicKey sender, String data, byte[] signature) {
+		try {
+			Signature ecdsaSignature = Signature.getInstance("ECDSA", "BC");
+			ecdsaSignature.initVerify(sender);
+			ecdsaSignature.update(data.getBytes());
+			return ecdsaSignature.verify(signature);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	public static final String generateHash(String data) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(data.getBytes());
+			StringBuilder hexaString = new StringBuilder();
+			for(int i = 0; i < hash.length; i++ ) {
+				String hex = Integer.toHexString(hash[i] & 0xff);
+				if(hex.length() == 1)hexaString.append('0');
+				hexaString.append(hex);
+			}
+			return hexaString.toString();
+		}catch(Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
