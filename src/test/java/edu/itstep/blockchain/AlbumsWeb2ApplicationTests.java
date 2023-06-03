d2c34230f8e34a1e822861bd9138cc9bcf6d394a
User <java.user.tester@gmail.com>
2023-06-03 11:57:44 +0300

    init project

---
+++ src/test/java/edu/itstep/blockchain/AlbumsWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itstep.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumsWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
